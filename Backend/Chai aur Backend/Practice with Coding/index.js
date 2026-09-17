c447de71606da37eee1475a9f475a4285450121d
MuhammadMuzamil-dev <zynovodigital@gmail.com>
2026-09-17 08:11:36 +0500

    Some new files and Notes Ve added to root folder

--- Backend/Chai aur Backend/Practice with Coding/index.js
+++ Backend/Chai aur Backend/Practice with Coding/index.js
@@ -1,2 +1,11 @@
-console.log("Chai aur Code");
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World! My name is khan");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
